78cc198c38de1d5db1ef42a89734d674e094dcd8
Heisemberg <heissen1122@gmail.com>
2023-08-15 13:03:58 -0500

    fin clase

--- pila.js
+++ pila.js
@@ -1,6 +1,50 @@
-class Node{
-    constructor(value){
-        this.value=value;
-        
+Pila a manera de array : class Stack{
+    constructor(){
+        this.items=[]
     }
+
+    isEmpty(){//Verificar si esta vacio
+        return this.items.length == 0;
+    }
+
+    size(){//Retornar tamaño
+        return this.items.length;
+    }
+
+    clear(){
+        this.items = []
+    }
+
+    push(item){//Ingresar un dato
+        this.items.push(item);
+    }
+
+    pop(){//Sacar un dato
+        if(this.isEmpty()){
+            return "La pila esta vacia";
+        }
+        return this.items.pop();
+    }
+    peek(){//Revisar el primer dato encontrado
+        if(this.isEmpty()){
+            return "La pila esta vacia"
+        }
+        return this.items[this.items.length - 1];
+
+    }
+
 }
+
+const stack = new Stack();
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.peek())
+stack.pop()
+console.log(stack.peek())
+stack.pop()
+console.log(stack.peek())
+stack.clear()
+console.log(stack.peek())
