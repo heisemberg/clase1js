b55e93e137a7f1a13dcb5a31fa9339a953aa352f
Heisemberg <heissen1122@gmail.com>
2023-08-15 11:53:42 -0500

    Actualización

---
+++ ejercicio3.js
@@ -0,0 +1,17 @@
+
+let limite = 578934
+let pares = 0
+let impares = 0
+let i = 0
+while ( i <= limite){
+    if (i % 2 == 0){
+        pares += i
+    }
+    else if (i % 2 != 0){
+        impares+= i
+        }
+    i += 1
+}
+
+console.log("la suma de pares es " + impares)
+console.log("La suma de pares es "+ pares)
